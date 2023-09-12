1fb2b1d1e185b9cbbc6956590d787e392d08a7e9
Safekarma <115760171+Safekarma@users.noreply.github.com>
2023-09-11 17:06:00 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-https://teachablemachine.withgoogle.com/models/qasUxXDxT/
+
 
 Webcam.set
 ({
@@ -25,4 +25,23 @@ calssifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models
 function modelLoaded()
 {
     console.log("modelLoaded");
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results)
+{
+    if(error)
+    {
+        console.error(error);
+    } else
+    {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3)*100 +"%";
+    }
 }
